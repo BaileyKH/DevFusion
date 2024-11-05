28105d96a3c0967122fb3d2f2d023062c41e6c84
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-11-05 13:08:28 -0700

    Updated: Adjusted nav to reflect user auth

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,17 +1,33 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../App"; 
+import { supabase } from "../supabaseDB"; 
 
 export const Nav = () => {
+  const navigate = useNavigate();
+  const user = useContext(UserContext); 
 
-    const navigate = useNavigate();
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error("Error signing out:", error);
+    } else {
+      navigate("/"); 
+    }
+  };
 
-    return(
-        <nav className="flex justify-between p-4 bg-primAccent text-white">
-            <Link to="/" className="text-2xl font-bold text-white">
-                DevFusion
-            </Link>
-            <div>
-                <button onClick={() => navigate('/auth')}>Sign In</button>
-            </div>
-        </nav>
-    );
-}
+  return (
+    <nav className="flex justify-between p-4 bg-primAccent text-white">
+      <Link to="/" className="text-2xl font-bold text-white">
+        DevFusion
+      </Link>
+      <div>
+        {user ? (
+          <button onClick={handleSignOut}>Sign Out</button>
+        ) : (
+          <button onClick={() => navigate("/auth")}>Sign In</button>
+        )}
+      </div>
+    </nav>
+  );
+};
