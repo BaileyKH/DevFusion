a39044918198a815a32abdfacf83483b60b3191d
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-11-08 13:11:25 -0700

    Feature: Nav now allows for user profile settings and signout capabilities

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,36 +1,65 @@
+import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { UserContext } from "../App"; 
-import { supabase } from "../supabaseDB"; 
+import { supabase } from "../supabaseDB";
+import { UserContext } from "../App";
+import { IconUserCircle } from '@tabler/icons-react';
+import { ProfileModal } from "./ProfileModal";
 
 export const Nav = () => {
+  const user = useContext(UserContext);
   const navigate = useNavigate();
-  const user = useContext(UserContext); 
+  const [isUserModalOpen, setIsUserModalOpen] = useState(false);
 
   const handleSignOut = async () => {
-    const { error } = await supabase.auth.signOut();
-    if (error) {
-      console.error("Error signing out:", error);
-    } else {
-      navigate("/"); 
+    if (user) {
+      await supabase.auth.signOut();
+      navigate('/auth');
     }
   };
 
   return (
-    <nav className="flex justify-between p-4 bg-primAccent text-white">
-      <Link to="/" className="text-2xl font-bold text-white">
-        DevFusion
-      </Link>
-      <div>
-        {user ? (
-          <div className="flex items-center gap-x-4">
-            <button onClick={() => navigate("/dashboard")}>Dashboard</button>
-            <button onClick={handleSignOut}>Sign Out</button>
-          </div>
-        ) : (
-          <button onClick={() => navigate("/auth")}>Sign In</button>
-        )}
-      </div>
-    </nav>
+    <>
+      <nav className="flex justify-between p-4 bg-secDark text-lightAccent shadow-md nav-gradient">
+        <Link to="/" className="text-2xl font-bold text-lightAccent">
+          DevFusion
+        </Link>
+        <div className="flex items-center space-x-4">
+          {user ? (
+            <>
+              {user.avatar_url ? (
+                <img
+                  src={user.avatar_url}
+                  alt="User Avatar"
+                  className="h-10 w-10 rounded-full cursor-pointer border border-darkAccent"
+                  onClick={() => setIsUserModalOpen(true)}
+                />
+              ) : (
+                <IconUserCircle
+                  size={28}
+                  className="text-white cursor-pointer"
+                  onClick={() => setIsUserModalOpen(true)}
+                />
+              )}
+              <button
+                onClick={handleSignOut}
+                className="bg-primAccent hover:bg-red-950 px-3 py-1 rounded-md transition duration-300"
+              >
+                Sign Out
+              </button>
+            </>
+          ) : (
+            <button
+              onClick={() => navigate('/auth')}
+              className="bg-primAccent hover:bg-red-950 px-3 py-1 rounded-md transition duration-300"
+            >
+              Sign In
+            </button>
+          )}
+        </div>
+      </nav>
+      {isUserModalOpen && (
+        <ProfileModal isOpen={isUserModalOpen} onClose={() => setIsUserModalOpen(false)} />
+      )}
+    </>
   );
 };
