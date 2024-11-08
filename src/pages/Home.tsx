6f4a4373aec367082ad662f3199ec66f9d82811a
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-11-08 13:31:52 -0700

    Update: Home ux adjustment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,13 +5,13 @@ export const Home= () => {
 
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-5xl font-bold mb-4">Welcome to Devfusion</h1>
-      <p className="text-lg mb-8">
+      <h1 className="text-5xl font-bold mb-4 text-lightAccent">Welcome to <span className='text-primAccent'>Devfusion</span></h1>
+      <p className="text-lg text-lightAccent mb-8">
         Collaborate with your team in real-time and manage your projects with ease.
       </p>
       <button
         onClick={() => navigate('/auth')}
-        className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow-lg"
+        className="px-6 py-3 bg-primAccent hover:bg-red-950 text-white rounded-lg shadow-md transition duration-300"
       >
         Get Started
       </button>
