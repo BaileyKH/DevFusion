567f03d1dc89b25e267368585bdbcdb86de3e9e0
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-11-09 10:25:40 -0700

    Update: Dependency updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        fira: ['"Fira Code"', 'monospace'],
-      },
-      colors: {
-        primDark: "#000000",
-        secDark: "#0f0f0f",
-        primAccent: "#931621",
-        lightAccent: "#c8c8c8",
-        darkAccent: "#b3b3b3",
-      },
-      backgroundImage: {
-        'radial-bg': 'radial-gradient(circle, #450a0a, #000000)',
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			fira: ['Fira Code"', 'monospace']
+  		},
+  		colors: {
+  			primDark: '#000000',
+  			secDark: '#0f0f0f',
+  			primAccent: '#931621',
+  			lightAccent: '#c8c8c8',
+  			darkAccent: '#b3b3b3'
+  		},
+  		backgroundImage: {
+  			'radial-bg': 'radial-gradient(circle, #450a0a, #000000)'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		animation: {
+  			'background-position-spin': 'background-position-spin 3000ms infinite alternate'
+  		},
+  		keyframes: {
+  			'background-position-spin': {
+  				'0%': {
+  					backgroundPosition: 'top center'
+  				},
+  				'100%': {
+  					backgroundPosition: 'bottom center'
+  				}
+  			}
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
