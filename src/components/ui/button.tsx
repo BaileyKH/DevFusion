17b57a2557d6e766033ab733f390b71a46068d9c
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-11-11 15:46:17 -0700

    Update: Reusable component style adjustment

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -10,14 +10,14 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-slate-900 text-slate-50 shadow hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90",
+          "bg-primAccent text-slate-50 shadow hover:bg-red-950/90 dark:text-slate-900 dark:hover:bg-slate-50/90",
         destructive:
-          "bg-red-500 text-slate-50 shadow-sm hover:bg-red-500/90 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/90",
+          "bg-red-500 text-slate-50 shadow-sm hover:bg-red-500/90",
         outline:
-          "border border-slate-200 bg-white shadow-sm hover:bg-slate-100 hover:text-slate-900 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+          "border border-primAccent bg-red shadow-sm hover:bg-red-100 hover:text-red-900",
         secondary:
-          "bg-slate-100 text-slate-900 shadow-sm hover:bg-slate-100/80 dark:bg-slate-800 dark:text-slate-50 dark:hover:bg-slate-800/80",
-        ghost: "hover:bg-slate-100 hover:text-slate-900 dark:hover:bg-slate-800 dark:hover:text-slate-50",
+          "bg-red-100 text-primDark shadow-sm hover:bg-red-100/80",
+        ghost: "hover:bg-red-100 hover:text-primAccent",
         link: "text-slate-900 underline-offset-4 hover:underline dark:text-slate-50",
       },
       size: {
