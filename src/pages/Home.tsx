dcaadd47bb7537b36079242bd359646195d5a223
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-11-18 10:59:29 -0700

    Update: home page ux adjustment

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import code from '@/assets/home/home-code.png'
 
 import { cn } from "@/lib/utils";
@@ -11,22 +13,22 @@ export const Home= () => {
 
   return (
     <main className=''>
-      <section className="relative flex gap-x-32 h-screen w-full items-center justify-center overflow-hidden rounded-lg mt-1">
+      <section className="relative flex gap-x-32 h-screen w-full items-center justify-center overflow-hidden rounded-lg">
           <div className="w-1/2 flex justify-end items-center">
               <div className="h-full w-full max-w-[32rem] items-center justify-center overflow-hidden pt-8 z-10">
-                  <BoxReveal boxColor={"#931621"} duration={1}>
+                  <BoxReveal boxColor={"#0398fc"} duration={1}>
                       <p className="text-8xl font-semibold text-primAccent">
                       DevFusion
                       </p>
                   </BoxReveal>
               
-                  <BoxReveal boxColor={"#931621"} duration={1}>
+                  <BoxReveal boxColor={"#0398fc"} duration={1}>
                       <h2 className="mt-4 text-2xl text-lightAccent">
                         Collaborate with your team in real-time and manage your projects with ease.
                       </h2>
                   </BoxReveal>
               
-                  <BoxReveal boxColor={"#931621"} duration={1}>
+                  <BoxReveal boxColor={"#0398fc"} duration={1}>
                       <div className="mt-[1.5rem]">
                       <p className='text-lightAccent text-lg'>
                           <span className='text-primAccent'>-&gt;</span> Real-Time chat with automatic code detection. <br />
@@ -36,13 +38,15 @@ export const Home= () => {
                       </div>
                   </BoxReveal>
               
-                  <BoxReveal boxColor={"#931621"} duration={0.5}>
-                    <Button className="mt-12 bg-primAccent py-6 text-lg tracking-wide">Get Started</Button>
+                  <BoxReveal boxColor={"#0398fc"} duration={0.5}>
+                    <Link to='/auth'>
+                      <Button className="mt-12 bg-primAccent py-6 text-lg tracking-wide transition duration-200">Get Started</Button>
+                    </Link>
                   </BoxReveal>
               </div>
           </div>
           <div className="w-1/2 flex justify-start items-center">
-            <NeonGradientCard neonColors={{ firstColor: '#fc0303', secondColor: '#f7b7b7' }} className="max-w-xl items-center justify-center ">
+            <NeonGradientCard neonColors={{ firstColor: '#0398fc', secondColor: '#bbe3fc' }} className="max-w-xl items-center justify-center ">
               <img src={code} className='w-full rounded-lg'/>
             </NeonGradientCard>
           </div>
